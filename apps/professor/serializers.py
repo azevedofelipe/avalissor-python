502e32c0aad6bf58f530eeab03761ea577217d48
azevedofelipe <felipe.azevedo@dna.tec.br>
2025-06-17 23:37:15 -0300

    Create serializers for professor, reviews and tags

---
+++ apps/professor/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import fields, serializers
+from apps.professor.models import Tag, Professor, ReviewProfessor
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ["id","name"]
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = ["id","campus","name","created_at","updated_at"]
+
+class ProfessorMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = ["id","name"]
+
+class ReviewProfessorSerializer(serializers.ModelSerializer):
+    professor = ProfessorMiniSerializer(read_only=True)
+    tags = TagSerializer(read_only=True, many=True)
+    
+    class Meta:
+        model = ReviewProfessor
+        fields = ["id","rating","professor","comment","tags","created_at","updated_at"]
+
