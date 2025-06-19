cf0298c80bda78a9d74c12d83f3dd6b5b8016065
azevedofelipe <felipe.azevedo@dna.tec.br>
2025-06-18 23:03:39 -0300

    Create Course model, link professors to specific courses

--- apps/professor/models.py
+++ apps/professor/models.py
@@ -8,9 +8,13 @@ class Tag(models.Model):
     def __str__(self):
         return str(self.name)
 
+class Course(models.Model):
+    # Maybe normalize names in future if needed
+    name = models.CharField(max_length=50, unique=True)
 
 class Professor(models.Model):
     campus = models.ForeignKey(Campus,on_delete=models.CASCADE)
+    courses = models.ManyToManyField(Course,related_name='professor')
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -20,6 +24,7 @@ class Professor(models.Model):
 
 class ReviewProfessor(models.Model):
     professor = models.ForeignKey(Professor,on_delete=models.CASCADE, related_name='professor')
+    course = models.ForeignKey(Course,on_delete=models.SET_NULL, null=True, blank=True)
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
     comment = models.CharField(max_length=300)
     tags = models.ManyToManyField(Tag, related_name="professors")
